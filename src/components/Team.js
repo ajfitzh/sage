2ab9fa0de755b1d96c6a15d1bdd6006f85521c6b
Austin James Fitzhugh <austin.j.fitzhugh@gmail.com>
2022-11-03 14:15:35 -0400

    Add files via upload

--- src/components/Team.js
+++ src/components/Team.js
@@ -16,7 +16,7 @@ let members = [
     bio: "Bret Dorgan is a Senior at ODU majoring in Computer Science. He is eager to help promote ODU with this project, and is looking forward to graduating in Spring 2023.",
     role: "Team Lead / DB Specialist / Backup Documentation Specialist",
     major: "Computer Science",
-    year: "2023",
+    year: "Senior",
     email: "bdorg001@odu.edu"
   },
   {
@@ -25,7 +25,7 @@ let members = [
     bio: "Austin is a senior at ODU, married with kids, and comes to this project with 5+ years experience in Advising and Coaching at the Community College level in Virginia. He is passionate about finding ways to connect students to better careers.",
     role: "SageScore Specialist / Testing / Backup UI/UX Specialist ",
     major: "CS",
-    year: "2023",
+    year: "Senior",
     email: "afitz010@odu.edu"
   },
   {
@@ -34,7 +34,7 @@ let members = [
     bio: "Benjamin Gonzalez is a senior at ODU studying computer science with a minor in cybersecurity. In his free time, he enjoys CrossFit, running, playing sports, and playing piano.",
     role: "Backend Specialist / Backup Testing Specialist",
     major: "CS",
-    year: "2023",
+    year: "Senior",
     email: "bgonz006@odu.edu"
   },
   {
@@ -43,7 +43,7 @@ let members = [
     bio:"Nathan Greaney is a senior at ODU majoring in Computer Science. He manages the IT Department for a division of his local municipality. After 25 long years in higher education, he will be graduating in Spring 2023. He plans to pursue a Masters in Computer Science in Fall 2023.",
     role: "No Role",
     major: "CS",
-    year: "2023",
+    year: "Senior",
     email: "ngrea001@odu.edu"
   },
   {
@@ -52,7 +52,7 @@ let members = [
     bio:"Marcus Jackson is a Senior majoring in Computer Science at Old Dominion University. He is a native of Mobile, Al and loves spending free time saltwater fishing.",
     role: "UI Specialist / Backup DB Specialist",
     major: "CS",
-    year: "2023",
+    year: "Senior",
     email: "mjack040@odu.edu"
   },
   {
@@ -61,7 +61,7 @@ let members = [
     bio: "Isaac Replogle is a Senior at ODU studying Computer Science and is currently employed full-time at EFT Corporation. In his free time, he enjoys going on road trips, with his wife Georgia, to visit family or to go on hiking trips in the mountains.",
     role: "Database Specialist / Webmaster",
     major: "CS",
-    year: "2023",
+    year: "Senior",
     email: "irepl001@odu.edu"
   },
   {
